78b8ab353e36c0e91a531de254f6a2ccc3b13c68
Koyashiro Kohaku <develop@koyashiro.net>
2020-06-07 05:48:17 +0900

    feat: add ChunkWriter

---
+++ src/PngMetaDataUtil/ChunkWriter.cs
@@ -0,0 +1,123 @@
+using System;
+using System.Buffers.Binary;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace KoyashiroKohaku.PngMetaDataUtil
+{
+    public class ChunkWriter
+    {
+        public static byte[] WriteImage(ReadOnlySpan<Chunk> chunks)
+        {
+            using var memoryStream = new MemoryStream();
+            using var binaryWriter = new BinaryWriter(memoryStream);
+
+            // [4 byte] : Signature
+            binaryWriter.Write(PngUtil.Signature);
+
+            Span<byte> length = stackalloc byte[4];
+
+            foreach (var chunk in chunks)
+            {
+                // [4 byte] : Length of ChunkData
+                BinaryPrimitives.WriteInt32BigEndian(length, chunk.ChunkDataLength);
+                binaryWriter.Write(length);
+
+                // [4 byte] : ChunkType
+                binaryWriter.Write(chunk.ChunkType.Value);
+
+                // [(length) byte] : ChunkData
+                binaryWriter.Write(chunk.ChunkData.Value);
+
+                // [4 byte] : CRC
+                binaryWriter.Write(new byte[4]);
+            }
+
+            return memoryStream.ToArray();
+        }
+
+        public static byte[] AddChunk(ReadOnlySpan<byte> image, params Chunk[] appendChunks)
+        {
+            var chunks = ChunkReader.GetChunks(image);
+            chunks.InsertRange(chunks.Count - 2, appendChunks);
+
+            using var memoryStream = new MemoryStream();
+            using var binaryWriter = new BinaryWriter(memoryStream);
+
+            binaryWriter.Write(PngUtil.Signature);
+
+            var length = new byte[4];
+
+            foreach (var chunk in chunks)
+            {
+                // [4 byte] : Length of ChunkData
+                BinaryPrimitives.WriteInt32BigEndian(length, chunk.ChunkDataLength);
+                binaryWriter.Write(length);
+
+                // [4 byte] : ChunkType
+                binaryWriter.Write(chunk.ChunkType.Value);
+
+                // [(length) byte] : ChunkData
+                binaryWriter.Write(chunk.ChunkData.Value);
+
+                // [4 byte] : CRC
+                binaryWriter.Write(new byte[4]);
+            }
+
+            return memoryStream.ToArray();
+        }
+
+        public static byte[] RemoveChunk(ReadOnlySpan<byte> image, params string[] chunkTypes)
+        {
+            var chunks = ChunkReader.GetChunks(image).Where(c => !chunkTypes.Any(ct => ct == c.ChunkType.ToString())).ToArray();
+
+            using var memoryStream = new MemoryStream();
+            using var binaryWriter = new BinaryWriter(memoryStream);
+
+            binaryWriter.Write(PngUtil.Signature);
+
+            var length = new byte[4];
+
+            foreach (var chunk in chunks)
+            {
+                // [4 byte] : Length of ChunkData
+                BinaryPrimitives.WriteInt32BigEndian(length, chunk.ChunkDataLength);
+                binaryWriter.Write(length);
+
+                // [4 byte] : ChunkType
+                binaryWriter.Write(chunk.ChunkType.Value);
+
+                // [(length) byte] : ChunkData
+                binaryWriter.Write(chunk.ChunkData.Value);
+
+                // [4 byte] : CRC
+                binaryWriter.Write(new byte[4]);
+            }
+
+            //for (int i = 0; i < chunks.Length; i++)
+            //{
+            //    var chunk = chunks[i];
+
+            //    // [4 byte] : Length of ChunkData
+            //    BinaryPrimitives.WriteInt32BigEndian(length, chunk.ChunkDataLength);
+            //    binaryWriter.Write(length);
+
+            //    // [4 byte] : ChunkType
+            //    binaryWriter.Write(chunk.ChunkType.Value);
+
+            //    // [(length) byte] : ChunkData
+            //    binaryWriter.Write(chunk.ChunkData.Value);
+
+            //    if (i+1 != chunks.Length)
+            //    {
+            //        // [4 byte] : CRC
+            //        binaryWriter.Write(new byte[4]);
+            //    }
+            //}
+
+            return memoryStream.ToArray();
+        }
+    }
+}
