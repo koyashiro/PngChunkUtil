b48fd6fa97a4ac2b1cc0ed24fba876dab14fb5e5
Koyashiro Kohaku <develop@koyashiro.net>
2020-06-11 16:20:08 +0900

    enable nullable

--- src/PngChunkUtil/ChunkWriter.cs
+++ src/PngChunkUtil/ChunkWriter.cs
@@ -1,10 +1,6 @@
 using System;
-using System.Buffers.Binary;
-using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Text;
-using Force.Crc32;
 
 namespace KoyashiroKohaku.PngChunkUtil
 {
