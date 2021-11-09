fcca18390cecb1bdc591cf2103a41bd7fb21d576
koyashiro <develop@koyashi.ro>
2021-11-09 21:13:33 +0900

    chore: add example project

---
+++ examples/ChunkReader/Program.cs
@@ -0,0 +1,26 @@
+using System;
+using System.IO;
+using System.Linq;
+
+using Koyashiro.PngChunkUtil;
+
+namespace Koyashiro.ChunkReader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var bytes = File.ReadAllBytes("example.png");
+            var chunks = PngReader.Parse(bytes);
+
+            foreach (var chunk in chunks)
+            {
+                var length = string.Format("{0,5}", chunk.ChunkDataLength);
+                var chunkType = chunk.ChunkType;
+                var chunkData = string.Join(", ", chunk.ChunkDataBytes.ToArray().Select(b => $"0x{b:x2}"));
+
+                Console.WriteLine($"Length: {length},    Chunk Type: {chunkType},    Chunk Data: [ {(chunkData.Length > 48 ? $"{chunkData.Remove(48)}..." : chunkData)} ]");
+            }
+        }
+    }
+}
